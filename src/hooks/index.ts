698c48c68997af29bb28acdf0ad2f10487dffcb0
YohanOspina7 <yohanospina64@gmail.com>
2026-03-06 19:49:21 -0500

    Revert "Filtrado y paginacion/ Añade el filtrado de la pagina de celulares"

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,4 +2,3 @@
 
 // PRODUCTOS
 export * from "./products/useProducts";
-export * from "./products/useFilteredProducts";
--- src/pages/CellPhonesPage.tsx
+++ src/pages/CellPhonesPage.tsx
@@ -1,21 +1,10 @@
 import { CardProduct } from "../components/products/CardProduct";
 import { prepareProducts } from "../components/helpers";
 import { ContainerFilter } from "../components/products/ContainerFilter";
-import { useFilteredProducts } from "../hooks";
-import { useState } from "react";
+import { useProducts } from "../hooks";
 
 export const CellPhonesPage = () => {
-  const [page, setPage] = useState(1);
-  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
-
-  const {
-    data: products,
-    isLoading,
-    totalProducts,
-  } = useFilteredProducts({
-    page,
-    brands: selectedBrands,
-  });
+  const { products, isLoading } = useProducts();
 
   if (isLoading || !products) return <p>Cargando...</p>;
 
@@ -27,34 +16,25 @@ export const CellPhonesPage = () => {
 
       <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-col-3 xl:grid-cols-5">
         {/* FILTROS */}
-        <ContainerFilter
-          setSelectedBrands={setSelectedBrands}
-          selectedBrands={selectedBrands}
-        />
+        <ContainerFilter />
 
-        {isLoading ? (
-          <div className="flex items-center justify-center col-span-2 h-[500px]">
-            <p className="text-2xl">Cargando...</p>
+        <div className="flex flex-col justify-center gap-12 lg:col-span-2 xl:col-span-4">
+          <div className="grid grid-cols-2 gap-3 gap-y-10 xl:grid-cols-4">
+            {preparedProducts.map((product) => (
+              <CardProduct
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                colors={product.colors}
+                img={product.images[0]}
+                slug={product.slug}
+                variants={product.variants}
+              />
+            ))}
           </div>
-        ) : (
-          <div className="flex flex-col justify-center gap-12 lg:col-span-2 xl:col-span-4">
-            <div className="grid grid-cols-2 gap-3 gap-y-10 xl:grid-cols-4">
-              {preparedProducts.map((product) => (
-                <CardProduct
-                  key={product.id}
-                  name={product.name}
-                  price={product.price}
-                  colors={product.colors}
-                  img={product.images[0]}
-                  slug={product.slug}
-                  variants={product.variants}
-                />
-              ))}
-            </div>
 
-            {/* TO DO: PAGINACION */}
-          </div>
-        )}
+          {/* TO DO: PAGINACION */}
+        </div>
       </div>
     </>
   );
