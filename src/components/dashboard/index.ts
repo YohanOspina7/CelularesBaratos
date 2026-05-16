f7a0c8a97f469e7effd38007067355a10c0a98b4
Yohan <yohanospina64@gmail.com>
2026-05-15 19:07:37 -0500

    Formulario-Poroducto/Esquema ZOD de producto

--- src/components/dashboard/index.ts
+++ src/components/dashboard/index.ts
@@ -5,3 +5,4 @@ export * from "./Sidebar";
 // PRODUCTOS
 export * from "./products/TableProduct";
 export * from "./products/CellTableProduct";
+export * from "./products/FormProduct"
---
+++ src/components/dashboard/products/FormProduct.tsx
@@ -0,0 +1,5 @@
+export const FormProduct = () => {
+  return (
+    <div>FormProduct</div>
+  )
+}
